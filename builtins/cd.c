3746261c47138e164fe62390d46c33ddff15b429
Anas Taai <ataai@e1r5p4.1337.ma>
2025-03-04 16:17:16 +0000

    working on cd still unfinished

--- builtins/cd.c
+++ builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: ataai <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 16:43:12 by ataai             #+#    #+#             */
-/*   Updated: 2025/03/03 22:31:08 by ataai            ###   ########.fr       */
+/*   Updated: 2025/03/04 16:16:41 by ataai            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,33 @@
 
 //just absolute for now
 
-int	cd(char *str)
+int	set_by_key(t_env **env, char *key, const char *val)
 {
-	if (str == NULL)
+	char	**pair;
+
+	pair = malloc(sizeof(char *) * 2);
+	if (pair == NULL)
 		return (-1);
-	chdir(str);
+	pair[0] = ft_strdup(key);
+	pair[1] = ft_strdup(val);
+	if (pair[0] == NULL || pair[1] == NULL)
+	{
+		free(pair[0]);
+		free(pair[1]);
+		free(pair);
+	}
+	export_add_replace(&env, pair);
+	return (0);
+}
+
+int	cd(t_env **env, char *str)
+{
+	char	wd[PATH_MAX];
+
+	if (str == NULL || chdir(str) == -1)
+		return (-1);
+	getcwd(wd, PATH_MAX);
+	set_by_key(env, "PWD", wd);
+	// to be continued
 	return (0);
 }
--- builtins/env.c
+++ builtins/env.c
@@ -6,7 +6,7 @@
 /*   By: ataai <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 10:42:46 by ataai             #+#    #+#             */
-/*   Updated: 2025/03/04 14:17:22 by ataai            ###   ########.fr       */
+/*   Updated: 2025/03/04 14:43:53 by ataai            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,99 +33,3 @@ int	print_env(t_env *env)
 	}
 	return (0);
 }
-
-void	set_flag(t_env *env, int f)
-{
-	while (env)
-	{
-		env->flag = f;
-		env = env->next;
-	}
-}
-
-int	node_lst_len(t_env *env)
-{
-	int	i;
-
-	i = 0;
-	while (env)
-	{
-		i++;
-		env = env->next;
-	}
-	return (i);
-}
-
-void	print_export(t_env *env)
-{
-	t_env	*tmp;
-	t_env	*small;
-	int	i;
-
-	set_flag(env, 0);
-	i = node_lst_len(env);
-	while (i)
-	{
-		tmp = env;
-		while (tmp && tmp->flag)
-			tmp = tmp->next;
-		small = tmp;
-		while (tmp)
-		{
-			if (tmp->key && ft_strcmp(small->key, tmp->key) > 0 && tmp->flag == 0)
-				small = tmp;
-			tmp = tmp->next;
-		}
-		small->flag = 1;
-		put_export(small);
-		i--;
-	}
-}
-
-int	put_export(t_env *env)
-{
-	if (env == NULL)
-		return (-1);
-	if (env->key)
-	{
-		if (ft_strcmp(env->key, "_") == 0)
-			return (0);
-		ft_putstr("declare -x ");
-		ft_putstr(env->key);
-	}
-	if (env->val)
-	{
-		ft_putstr("=\"");
-		ft_putstr(env->val);
-		ft_putstr("\"");
-	}
-	ft_putstr("\n");
-	return (0);
-}
-
-//int	print_export(t_env *env)
-//{
-//	t_env	*tmp;
-//
-//	if (env == NULL)
-//		return (-1);
-//	sort_env(env);
-//	tmp = env;
-//	while (tmp)
-//	{
-//		if (tmp->key)
-//		{
-//			ft_putstr("declare -x ");
-//			ft_putstr(tmp->key);
-//		}
-//		if (tmp->val)
-//		{
-//			ft_putstr("=\"");
-//			ft_putstr(tmp->val);
-//			ft_putstr("\"");
-//		}
-//		ft_putstr("\n");
-//		tmp = tmp->next;
-//	}
-//	return (0);
-//}
